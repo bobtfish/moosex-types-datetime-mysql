fc68b31715be3bdc883d2b7657df59eb8f8d31bd
Tomas Doran <bobtfish@bobtfish.net>
2010-08-13 17:27:52 +0100

    Skeleton

---
+++ lib/MooseX/Types/DateTime/MySQL.pm
@@ -0,0 +1,26 @@
+package MooseX::Types::DateTime::MySQL;
+use Moose;
+use namespace::autoclean;
+
+1;
+
+=head1 NAME
+
+MooseX::Types::DateTime::MySQL - 
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head1 BUGS
+
+=head1 AUTHOR
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 the above author(s).
+
+This sofware is free software, and is licensed under the same terms as perl itself.
+
+=cut
+
